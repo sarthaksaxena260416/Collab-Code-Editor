b90d35ee292d7819ad3cfd8047d485ed851138c9
Sarthak <sarthaksaxena2604@gmail.com>
2026-05-14 15:17:20 +0530

    fix: minimal middleware

--- client/middleware.ts
+++ client/middleware.ts
@@ -1,18 +1,5 @@
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
-import { NextResponse } from 'next/server';
-
-const isPublicRoute = createRouteMatcher([
-  '/sign-in(.*)',
-  '/sign-up(.*)',
-  '/api(.*)',
-]);
-
-export default clerkMiddleware(async (auth, request) => {
-  if (!isPublicRoute(request)) {
-    await auth.protect();
-  }
-});
+export { default } from '@clerk/nextjs/server';
 
 export const config = {
-  matcher: ['/((?!_next/static|_next/image|favicon.ico).*)'],
+  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
 };
